0ef9fc159b8fa2ea072b1790c0a89b43f6fa4bd4
Sean Olson <srolson@callutheran.edu>
2019-07-12 18:44:19 -0500

    added pics, GET req, pics/like btn rendering to DOM

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,14 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat1.png', description: 'Ima goofy lil goater', likes: 0 },
+    { id: 2, path: 'images/goat2.jpg', description: 'Goat brothers. Sharing a blanket. Sharing a moment.', likes: 0 },
+    { id: 3, path: 'images/goat3.jpg', description: 'Backstreet Goat, making the lady goats go cray', likes: 0 },
+    { id: 4, path: 'images/goat4.jpg', description: 'Pondering goat. Dreaming about a lil goat snack', likes: 0 },
+    { id: 5, path: 'images/goat5.jpeg', description: 'Goat Photobomb', likes: 0 },
+    { id: 6, path: 'images/goat6.jpeg', description: 'This goat just found out he got into college.', likes: 0 },
+    { id: 7, path: 'images/goat7.png', description: 'This goat takes no bull', likes: 0 },
+    { id: 8, path: 'images/goat8.jpg', description: 'Feeling cute. Might delete later idk.', likes: 0 },
+    { id: 9, path: 'images/goat9.jpg', description: 'Justin Timbergoat', likes: 0 },
+
 ];
 
 module.exports = galleryItems;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,57 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  
+  state = {
+    goatList: [],
+  }
+
+  componentDidMount() {
+    this.getGoats();
+  }
+
+  getGoats = () => {
+    console.log('in getGoats');
+    axios.get('/gallery')
+    .then( response => {
+      this.setState({
+        goatList: response.data,
+      })
+    }).catch(error => {
+      console.log('error getting goats to frolic back to the client', error);
+    })
+  }
+
+  handleLike = (item) => {
+    let count = item.likes;
+    axios.put(`/gallery/like/${item.id}`,{likes: count})
+    .then((response) => {
+      console.log(response);
+      this.getGoats();
+    }).catch((error) => {
+      console.log('error updating likes');
+    })
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Gallery of my life</h1>
+          <h1 className="App-title">#GoatLife</h1>
         </header>
         <br/>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <p>Gallery of Goats.</p>
+        <ul>{this.state.goatList.map((item) =>
+            <li  key={item.id}>  <img src={item.path} alt={item.description}/>
+            <br/>
+            {item.likes}
+                  <button className="button" key={item.id} onClick = {() => {this.handleLike(item)}}>Like</button>
+            </li>
+                  )}
+        </ul>
       </div>
     );
   }
